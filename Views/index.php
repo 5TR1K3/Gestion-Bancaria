5ac86c580553d77fcf56f38aab5542748f4d5d87
MarvinArtiga <marvin.artigagonzalez@gmail.com>
2024-03-19 23:07:34 -0600

    agregar vistas

--- Views/index.php
+++ Views/index.php
@@ -1,62 +1,34 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Main CSS-->
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url; ?>Assets/css/main.css">
-    <!-- Font-icon css-->
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url; ?>Assets/css/font-awesome.min.css">
-    <title>Iniciar | Sesión</title>
-</head>
-
-<body>
-    <section class="material-half-bg">
-        <div class="cover"></div>
-    </section>
-    <section class="login-content">
-        <div class="logo">
-            <h1>Bienvenido</h1>
-        </div>
-        <div class="login-box">
-            <form class="login-form" id="frmLogin" onsubmit="frmLogin(event);">
-                <h3 class="login-head"><i class="fa fa-lg fa-fw fa-user"></i>Iniciar Sesión</h3>
-                <div class="form-group">
-                    <label class="control-label">USUARIO</label>
-                    <input class="form-control" type="text" placeholder="Usuario" id="usuario" name="usuario" autofocus required>
-                </div>
-                <div class="form-group">
-                    <label class="control-label">CONTRASEÑA</label>
-                    <input class="form-control" type="password" placeholder="Contraseña" id="clave" name="clave" required>
-                </div>
-                <div class="alert alert-danger d-none" role="alert" id="alerta">
-                    
-                </div>
-                <div class="form-group btn-container">
-                    <button class="btn btn-primary btn-block" type="submit"><i class="fa fa-sign-in fa-lg fa-fw"></i>Login</button>
-                </div>
-            </form>
-        </div>
-    </section>
-    <!-- Essential javascripts for application to work-->
-    <script src="<?php echo base_url; ?>Assets/js/jquery-3.6.0.min.js"></script>
-    <script src="<?php echo base_url; ?>Assets/js/bootstrap.bundle.min.js"></script>
-    <script src="<?php echo base_url; ?>Assets/js/main.js"></script>
-    <!-- The javascript plugin to display page loading on top-->
-    <script src="<?php echo base_url; ?>Assets/js/pace.min.js"></script>
-    <script>
-        const base_url = '<?php echo base_url; ?>';
-    </script>
-    <script src="<?php echo base_url; ?>Assets/js/login.js"></script>
-    <script type="text/javascript">
-        // Login Page Flipbox control
-        $('.login-content [data-toggle="flip"]').click(function() {
-            $('.login-box').toggleClass('flipped');
-            return false;
-        });
-    </script>
-</body>
-
-</html>
+<?php
+    require_once "Config/Config.php";
+    $ruta = !empty($_GET['url']) ? $_GET['url'] : "Home/index";
+    $array = explode("/", $ruta);
+    $controller = $array[0];
+    $metodo = "index";
+    $parametro = "";
+    if (!empty($array[1])) {
+        if (!empty($array[1] != "")) {
+            $metodo = $array[1];
+        }
+    }
+    if (!empty($array[2])) {
+        if (!empty($array[2] != "")) {
+            for ($i=2; $i < count($array); $i++) { 
+                $parametro .= $array[$i]. ",";
+            }
+            $parametro = trim($parametro, ",");
+        }
+    }
+    require_once "Config/App/Autoload.php";
+    $dirControllers = "Controllers/".$controller.".php";
+    if (file_exists($dirControllers)) {
+        require_once $dirControllers;
+        $controller = new $controller();
+        if (method_exists($controller, $metodo)) {
+            $controller->$metodo($parametro);
+        }else{
+            header('Location:' . base_url . 'Configuracion/Error');
+        }
+    }else{
+        header('Location:' . base_url . 'Configuracion/Error');
+    }
+?>
