39a1d2ff261c632781c7f86e4ebb42180307be38
MarvinArtiga <marvin.artigagonzalez@gmail.com>
2024-03-23 19:40:58 -0600

    eliminar lo que no sirve

--- Config/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-const base_url = "http://localhost/DSS02L/GestionBancaria/"; //Si usas el puerto 81 agregar al localhost:81
-const host = "localhost";
-const user = "root";
-const pass = "";
-const db = "NoHay";
-const charset = "charset=utf8";
-?>
